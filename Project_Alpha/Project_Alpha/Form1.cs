2988fbb22415bd29a7d6d304fe4b0f32e2cabda8
NeoKms <upachko@gmail.com>
2018-12-17 22:44:13 +1000

    add logs in BD

--- Project_Alpha/Project_Alpha/Form1.cs
+++ Project_Alpha/Project_Alpha/Form1.cs
@@ -32,17 +32,24 @@ namespace Project_Alpha
             {
                 this.label1.Text = "Connecting to server...";
                 conn.Open();
-                string sql = "SELECT login, password FROM login";
-                MySqlCommand cmd = new MySqlCommand(sql, conn);
+                MySqlCommand cmd = new MySqlCommand();
+                cmd.CommandText = "SELECT login, password, ID_worker, ID_factory FROM login";
+                cmd.Connection = conn;
                 MySqlDataReader rdr = cmd.ExecuteReader();
                 while (rdr.Read())
                 {
                     if (rdr[0].ToString() == this.textBox1.Text && rdr[1].ToString() == this.textBox2.Text){
                         this.label1.Text = "Done";
+                        DateTime now = DateTime.Now;
+                        cmd.CommandText = "INSERT INTO `project_1`.`logs` (`time_when`, `ID_worker`, `ID_factory`) VALUES (\'" + now.ToString("yyyy/MM/dd HH:mm:ss") + "\', \'" + rdr[2] + "\', \'" + rdr[3] + "\');";
+                        rdr.Close();
+                        cmd.ExecuteNonQuery();
+                        break;
                     }
                     else
                     {
                         this.label1.Text = "Login or password not correct. Retry again.";
+
                     }
                 }
                 rdr.Close();
@@ -53,7 +60,7 @@ namespace Project_Alpha
                 MessageBox.Show(ex.ToString());
             }
             conn.Close();
-           //this.label1.Text = "Done.";
+           
 
         }
     }
