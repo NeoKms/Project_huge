aea8059c1268200a69ec9d4ffbc50518fe1e34a0
NeoKms <upachko@gmail.com>
2018-12-17 14:45:23 +1000

    Connection database

---
+++ Project_Alpha/Project_Alpha/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
+namespace Project_Alpha
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string connStr = "server=127.0.0.1;user=Admin;database=Project_1;port=3306;password=root";
+            MySqlConnection conn = new MySqlConnection(connStr);
+            try
+            {
+                MessageBox.Show("Connecting to MySQL...");
+                conn.Open();
+                // Perform database operations
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+            conn.Close();
+            MessageBox.Show("Done.");
+        }
+    }
+}
