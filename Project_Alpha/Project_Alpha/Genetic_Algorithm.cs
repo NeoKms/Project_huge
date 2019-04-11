7041a665a05ba04ddd94a2a17805da16dd470d33
NeoKms <upachko@gmail.com>
2019-04-12 00:26:25 +1000

    add new class genetic_algorithm, alpha

---
+++ Project_Alpha/Project_Alpha/Genetic_Algorithm.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
+namespace Project_Alpha
+{
+    class Genetic_Algorithm
+    {
+        public int ID_FACTORY;
+        MySqlConnection CON_BD;
+
+        public Genetic_Algorithm(int id_fac)
+        {
+            ID_FACTORY = id_fac;
+            CON_BD = new MySqlConnection("server=127.0.0.1;user=Admin;database=Project_1;port=3306;password=root");
+        }
+
+        class Individ
+        {
+            public Individ()
+            {
+
+            }
+        }
+    }
+}
